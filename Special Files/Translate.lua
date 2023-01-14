ad302299465b7fcc3ca15a9adb5144c66a903974
Angel <100471435+AngellessS@users.noreply.github.com>
2023-01-14 15:28:52 +1000

    Update Translate.lua

--- Special Files/Translate.lua
+++ Special Files/Translate.lua
@@ -1,1 +1,11 @@
+function Translate(text, selected_language, target_language)
+	for i = 1,#EnglishTable do
+		if text == RussianTable[i] and target_language == "English(Transated)" then
+			text = EnglishTable[i]
+		elseif text == EnglishTable[i] and target_language == "Русский(Translated)" then
+			text = RussianTable[i]
+		end
+	end
+	return text
+end
 
