eb6f087106fa3ef050afe06bdeb61850d54f2bca
Angel <100471435+AngellessS@users.noreply.github.com>
2023-01-16 02:07:25 +1000

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,13 @@
 <?php
-  $keys_array = array('somekey','secondkey');
+	function ExitAlert($msg){
+    exit("gg.alert('".$msg."')");
+	}
+	$JDecode = json_decode(file_get_contents('php://input'),true); 
+	$keys_array = array('test','somekey');
+	$get_key = $JDecode["InputKey"]
+	if(array_key_exist($get_key, $keys_array)) {
+		ExitAlert("Key Exist");
+	} else {
+		ExitAlert("Key Not Exist");
+	}
 ?>
